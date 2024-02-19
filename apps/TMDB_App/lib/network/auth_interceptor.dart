c67b7d4d8bce71e03eaea2667370605a0e692404
Harshal Chaudhari <harshal.chaudhari@pharmarack.com>
2024-02-19 10:23:54 +0530

    Update splash logo

--- apps/TMDB_App/lib/network/auth_interceptor.dart
+++ apps/TMDB_App/lib/network/auth_interceptor.dart
@@ -3,8 +3,7 @@ import "package:dio/dio.dart";
 class AuthInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    const String _token =
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Y2YzOTNjMTlhYjg4MTJkNjc0MGRlMmVjNjI4MjQwNCIsInN1YiI6IjVlNjFmYmQ1MjJhZjNlMDAxM2RjZGU2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vsaBkew5PUzysWcV78QE5rjMLqDb1ubLcdFW9G3atpc";
+    const String _token = "";
 
     options.headers["Authorization"] = _token;
 
